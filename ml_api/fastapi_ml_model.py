89efce91ddc219b13324d18cc1ac46df8ec510a7
vidya-ratan <74319341+vidya-ratan@users.noreply.github.com>
2023-12-17 18:09:33 +0530

    Update fastapi_ml_model.py

--- ml_api/fastapi_ml_model.py
+++ ml_api/fastapi_ml_model.py
@@ -12,40 +12,40 @@ import pandas as pd
 import os
 
 # Print the current working directory
-print("Current working directory:", os.getcwd())
-# dataset = pd.read_csv('Salary_Data.csv')
-# X = dataset.iloc[:, :-1].values #get a copy of dataset exclude last column
-# y = dataset.iloc[:, 1].values #get array of dataset in column 1st
+# print("Current working directory:", os.getcwd())
+dataset = pd.read_csv('ml_api/Salary_Data.csv')
+X = dataset.iloc[:, :-1].values #get a copy of dataset exclude last column
+y = dataset.iloc[:, 1].values #get array of dataset in column 1st
 
-# # Splitting the dataset into the Training set and Test set
-# from sklearn.model_selection import train_test_split 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
 
-# from sklearn.linear_model import LinearRegression
-# regressor = LinearRegression()
-# regressor.fit(X_train, y_train)
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
-# # class PredictionInput(BaseModel):
-# #     feature_values: list
+# class PredictionInput(BaseModel):
+#     feature_values: list
 
-# @app.get("/predict")
-# def predict():
-#     try:
-#         # # Convert input data to NumPy array
-#         # features = np.array(input_data.feature_values).reshape(1, -1)
+@app.get("/predict")
+def predict():
+    try:
+        # # Convert input data to NumPy array
+        # features = np.array(input_data.feature_values).reshape(1, -1)
 
-#         # # Make prediction using the pre-trained model
-#         # prediction = model.predict(features)[0]
+        # # Make prediction using the pre-trained model
+        # prediction = model.predict(features)[0]
 
-#         y_pred = regressor.predict(X_test).tolist()
+        y_pred = regressor.predict(X_test).tolist()
 
-#         return {"prediction": y_pred}
+        return {"prediction": y_pred}
 
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-# if __name__ == "__main__":
-#     import uvicorn
+if __name__ == "__main__":
+    import uvicorn
 
-#     # Run the FastAPI application using Uvicorn
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+    # Run the FastAPI application using Uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
